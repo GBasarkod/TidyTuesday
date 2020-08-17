04eae7acab3afb00fdb666e2cebb50e40a149af1
Geet <geetanjali73@gmail.com>
2020-08-17 11:04:31 +1000

    Update Avatar.R

--- Avatar.R
+++ Avatar.R
@@ -1,3 +1,4 @@
+#Load libraries
 library(tidytuesdayR)
 library(tidyverse)
 library(tidytext)
@@ -6,37 +7,41 @@ library(ggplot2)
 library(tvthemes)
 library(extrafont)
 
+#Load data
 tuesdata <- tidytuesdayR::tt_load('2020-08-11')
 avatar <- tuesdata$avatar
 
+#Tidy data and create variables needed
 avatar %>% 
-  filter(character == "Zuko") %>% 
-  select(book_num, chapter_num, character_words) %>% 
+  filter(character == "Zuko") %>% #Only keep lines spoken by Zuko
+  select(book_num, chapter_num, character_words) %>% #select columns required
   group_by(book_num, chapter_num) %>% 
-  mutate(linenumber = row_number()) %>% 
+  mutate(linenumber = row_number()) %>% #Label the lines spoken by Zuko by book and chapter 
   ungroup() %>% 
-  unnest_tokens(output = word, input = character_words, token = "words")  %>% 
-  filter(word %in%  c("avatar", "aang", "aang's", "avatar's")) %>% 
+  unnest_tokens(output = word, input = character_words, token = "words")  %>% #add column with each row = one word spoken by Zuko
+  filter(word %in%  c("avatar", "aang", "aang's", "avatar's")) %>% #Only retain these words 
   mutate(av = 
            case_when(word == "avatar"  ~ 1,
-                     word == "avatar's" ~ 1)) %>% 
+                     word == "avatar's" ~ 1)) %>% #Create a column where a value of 1 is given to each instance of the word Avatar (NA for Aang)
   mutate(aa = 
            case_when(word == "aang"  ~ 1,
-                     word == "aang's" ~ 1)) %>% 
+                     word == "aang's" ~ 1)) %>% #Create a column where a value of 1 is given to each instance of the word Aang (NA for Avatar)
   group_by(book_num, chapter_num) %>% 
   summarise(Avatar = sum(av),
-            Aang = sum(aa)) %>% 
-  gather(key = "Name", value = "Frequency", Avatar, Aang) %>% 
+            Aang = sum(aa)) %>% #count the number of occurences for each name within each chapter
+  gather(key = "Name", value = "Frequency", Avatar, Aang) %>% #change to long format, creating one column that has a factor variable with the two names, and a second column with the values
   ungroup() %>% 
   mutate(book_num =
            factor(book_num,
-                  labels = c("Water", "Earth", "Fire"))) -> avatar
+                  labels = c("Water", "Earth", "Fire"))) -> avatar #Change value of book factors to the names of the books (actually, there was a variable with book names already, so just use that one instead)
 
-extrafont::font_import("~/tvthemes/fonts/Slayer",prompt = FALSE)
+extrafont::font_import("~/tvthemes/fonts/Slayer",prompt = FALSE) #Load specific font that is downloaded to your system.
 extrafont::fonts()
 
+
+#Create plot
 ggplot(avatar, aes(x=chapter_num, y=Frequency, fill = Name)) +
-  geom_bar(position = "stack", stat = "identity", width = .8) +
+  geom_bar(position = "stack", stat = "identity", width = .8) + 
   facet_wrap(~book_num) +
   xlab("Chapter") +
   labs(title = "Avatar: The Last Airbender", subtitle = "The number of times Zuko mentions Avatar/Aang") +
